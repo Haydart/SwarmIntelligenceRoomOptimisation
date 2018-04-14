546dc078aa88c69fe16a7f79d7e94eabbc7402dd
Haydart <makowiecki.radoslaw@gmail.com>
2018-04-14 12:49:07 +0200

    Implement moving the recessive individual to dominant one

--- src/swarm/FireflyAlgorithm.kt
+++ src/swarm/FireflyAlgorithm.kt
@@ -2,6 +2,7 @@ package swarm
 
 import model.FurniturePiece
 import model.Room
+import testing.RastriginTest
 import java.lang.Math.random
 import kotlin.math.exp
 
@@ -10,20 +11,22 @@ import kotlin.math.exp
  */
 class FireflyAlgorithm {
 
-    val alpha = 1f
-    val beta = 1f
-    val gamma = 1f
+    val alpha = 0.8f
+    val beta = 0.8f
+    val gamma = 0.001f
 
-    val populationSize = 50
+    val populationSize = 200
     val furnitureCount = 5
-    val generationCount = 100
+    val generationCount = 1000
     val roomWidth = 150f
     val roomHeight = 100f
 
     val population: MutableList<Individual> = mutableListOf()
+    val testFunction = RastriginTest()
 
     fun runOptimisation() {
         generateInitialPopulation()
+        evaluatePopulation()
 
         var iteration = 0
 
@@ -31,26 +34,22 @@ class FireflyAlgorithm {
             (0 until populationSize).forEach { i ->
                 (0 until populationSize).forEach { j ->
                     if (population[j].intensity > population[i].intensity) {
+//                        println("Moving $i towards $j")
+//                        println(population[i].coords)
                         moveTowards(population[i], population[j])
+//                        println(population[i].coords)
+                        evaluateIndividual(population[i])
                     }
                 }
             }
-        }
-    }
-
-    private fun moveTowards(recessive: Individual, dominant: Individual) {
-        val distance = recessive.distanceTo(dominant)
-        val dominantIndividualAttractiveness = beta * exp(-gamma * distance)
-
-        recessive.coords.forEachIndexed { index, (x, y) ->
-            val newX = x + (dominantIndividualAttractiveness * (dominant.coords[index].first - x) + alpha * random()).toFloat()
-            val newY = y + (dominantIndividualAttractiveness * (dominant.coords[index].second - y) + alpha * random()).toFloat()
-            recessive.coords[index] = Pair(newX, newY)
+            collectStatistics()
+            iteration++
+//            println("_________")
         }
     }
 
     private fun generateInitialPopulation() {
-        var room = initRoom()
+        val room = initRoom()
 
         (0 until populationSize).forEach {
             population.add(Individual(room))
@@ -63,10 +62,48 @@ class FireflyAlgorithm {
         val furnitureList = mutableListOf<FurniturePiece>()
         furnitureList.add(FurniturePiece(20f, 15f))
         furnitureList.add(FurniturePiece(25f, 10f))
-        furnitureList.add(FurniturePiece(10f, 5f))
-        furnitureList.add(FurniturePiece(17f, 12f))
-        furnitureList.add(FurniturePiece(10f, 10f))
+//        furnitureList.add(FurniturePiece(10f, 5f))
+//        furnitureList.add(FurniturePiece(17f, 12f))
+//        furnitureList.add(FurniturePiece(10f, 10f))
 
         return Room(furnitureList, roomWidth, roomHeight)
+    }
+
+    private fun moveTowards(recessive: Individual, dominant: Individual) {
+        val distance = recessive.distanceTo(dominant)
+        val dominantIndividualAttractiveness = beta * exp(-gamma * distance)
+
+        recessive.coords.forEachIndexed { index, (x, y) ->
+            val newX = x + (dominantIndividualAttractiveness * (dominant.coords[index].first - x) + alpha * (2 * random() - 1).toFloat())
+            val newY = y + (dominantIndividualAttractiveness * (dominant.coords[index].second - y) + alpha * (2 * random() - 1).toFloat())
+            recessive.coords[index] = Pair(newX, newY)
+        }
+    }
+
+    fun evaluatePopulation() {
+        population.forEach {
+            it.intensity = 1 / testFunction.evaluateIndividual(it)
+            println("Individual intensity: ${it.intensity}")
+        }
+    }
+
+    fun evaluateIndividual(individual: Individual) {
+        val priorIntensity = individual.intensity
+        individual.intensity = 1 / testFunction.evaluateIndividual(individual)
+//        println("Posterior intensity: ${individual.intensity}, prior intensity: $priorIntensity")
+    }
+
+    fun collectStatistics() {
+        var bestScore = 0f
+        var bestIndividual: Individual? = null
+        population.forEach {
+            if (it.intensity > bestScore) {
+                bestScore = it.intensity
+                bestIndividual = it
+            }
+        }
+
+        println(bestIndividual?.coords)
+        println("Best score ${bestIndividual?.intensity}")
     }
 }
